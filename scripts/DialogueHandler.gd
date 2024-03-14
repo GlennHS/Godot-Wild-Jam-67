f5b28d7c82cba21df0f23668d35f6a147e4bdccb
GlennHS <glenniumhs@gmail.com>
2024-03-14 22:12:15 +0000

    Add UI for Dialogue, add dialogue sccript starter.

---
+++ scripts/DialogueHandler.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+var current_dialogue = [
+	"This is some dialogue.",
+	"Here's more dialogue.",
+	"And even more!",
+	"",
+	"Did you like that empty dialogue?",
+	"No? Well too bad."
+]
+var position_in_dialogue = 0
+
+func show_dialogue():
+	# Show element
+	# Pause game
+	pass
+	
+func hide_dialogue():
+	# Hide element
+	# Unpause game
+	pass
+	
+func progress_dialogue():
+	pass
+	
+func set_dialogue_stream():
+	pass
+
+func update_dialogue_text():
+	pass
+	
+func update_speaker_image():
+	pass
