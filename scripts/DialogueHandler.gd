ce1e9cd3fc78da15bd31f94d0595cca5e7054b21
GlennHS <glenniumhs@gmail.com>
2024-03-14 22:33:34 +0000

    Dialogue system added.

--- scripts/DialogueHandler.gd
+++ scripts/DialogueHandler.gd
@@ -1,33 +1,84 @@
 extends CanvasLayer
 
-var current_dialogue = [
-	"This is some dialogue.",
-	"Here's more dialogue.",
-	"And even more!",
-	"",
-	"Did you like that empty dialogue?",
-	"No? Well too bad."
+class DialogueItem:
+	var dialogue: String = ""
+	var speaker_name: String = ""
+	var speaker_image_path: String = ""
+	
+func new_dialogue_item(obj) -> DialogueItem:
+	var r = DialogueItem.new()
+	r.dialogue = obj.dialogue
+	r.speaker_name = obj.speaker_name
+	r.speaker_image_path = obj.speaker_image_path
+	return r
+
+var current_dialogue: Array[DialogueItem] = [
+	new_dialogue_item({
+		"dialogue": "This is some dialogue.",
+		"speaker_name": "CHARLES",
+		"speaker_image_path": "res://sprites/ui_sprites/player_faces/headshot.png"
+	}),
+	new_dialogue_item({
+		"dialogue": "Here's more dialogue.",
+		"speaker_name": "CHARLES",
+		"speaker_image_path": "res://sprites/ui_sprites/player_faces/headshot.png"
+	}),
+	new_dialogue_item({
+		"dialogue": "And even more!",
+		"speaker_name": "CHARLES",
+		"speaker_image_path": "res://sprites/ui_sprites/player_faces/headshot.png"
+	}),
+	new_dialogue_item({
+		"dialogue": "",
+		"speaker_name": "CHARLES",
+		"speaker_image_path": "res://sprites/ui_sprites/player_faces/headshot.png"
+	}),
+	new_dialogue_item({
+		"dialogue": "Did you like that empty dialogue?",
+		"speaker_name": "CHARLES",
+		"speaker_image_path": "res://sprites/ui_sprites/player_faces/headshot.png"
+	}),
+	new_dialogue_item({
+		"dialogue": "No? Well too bad.",
+		"speaker_name": "CHARLES",
+		"speaker_image_path": "res://sprites/ui_sprites/player_faces/headshot.png"
+	})
 ]
 var position_in_dialogue = 0
 
+func _ready():
+	show_dialogue()
+
+func _input(event: InputEvent) -> void:
+	if InputEventMouseButton and event.is_pressed():
+		progress_dialogue()
+
 func show_dialogue():
-	# Show element
-	# Pause game
-	pass
+	get_tree().paused = true
+	visible = true
 	
 func hide_dialogue():
-	# Hide element
-	# Unpause game
-	pass
+	get_tree().paused = false
+	visible = false
 	
 func progress_dialogue():
+	position_in_dialogue += 1
+	if position_in_dialogue < current_dialogue.size():
+		update_dialogue_UI()
+	else:
+		hide_dialogue()
+	
+func set_dialogue_stream(new_dialogue_stream):
 	pass
 	
-func set_dialogue_stream():
-	pass
+func get_current_dialogue():
+	return current_dialogue[position_in_dialogue]
 
-func update_dialogue_text():
-	pass
+func update_dialogue_UI():
+	var d: DialogueItem = get_current_dialogue()
+	var image = Image.load_from_file(d.speaker_image_path)
+	var speaker_texture = ImageTexture.create_from_image(image)
 	
-func update_speaker_image():
-	pass
+	$DialogueContainer/HBoxContainer/DialogueText.text = d.dialogue
+	$DialogueContainer/HBoxContainer/VBoxContainer/SpeakerName.text = d.speaker_name
+	$DialogueContainer/HBoxContainer/VBoxContainer/SpeakerImage.texture = speaker_texture
---
+++ scripts/_types/Dialogue.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name Dialogue
+
+var dialogue: String = ""
+var speaker_name: String = ""
+var speaker_image_path: String = ""
